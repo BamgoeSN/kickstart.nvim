fb12645c69512ac9758805ec0146799737f03ee1
bamgoesn orca <60645387+BamgoeSN@users.noreply.github.com>
2024-01-27 22:17:49 +0900

    Add neovide

--- init.lua
+++ init.lua
@@ -440,3 +440,40 @@ end, 0)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- Neovide configuration
+if vim.g.neovide then
+  vim.cmd('TransparentDisable')
+  vim.o.guifont = "FiraCode Nerd Font:h12"
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_refresh_rate = 180
+  vim.g.neovide_scroll_animation_length = 0.05
+  vim.g.neovide_scroll_animation_far_lines = 99999999
+  vim.g.neovide_cursor_animation_length = 0.05
+  vim.g.neovide_cursor_trail_size = 0.5
+  vim.g.neovide_cursor_vfx_mode = "torpedo"
+  vim.g.neovide_cursor_vfx_particle_density = 10.0
+
+  -- Disable IME when not in insert mode
+  local function set_ime(args)
+    if args.event:match("Enter$") then
+      vim.g.neovide_input_ime = true
+    else
+      vim.g.neovide_input_ime = false
+    end
+  end
+
+  local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+  vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+    group = ime_input,
+    pattern = "*",
+    callback = set_ime
+  })
+
+  vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+    group = ime_input,
+    pattern = "[/\\?]",
+    callback = set_ime
+  })
+end
