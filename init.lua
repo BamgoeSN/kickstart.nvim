b63076586f0966a3b10dde1422bffa5c5f09b0ea
bamgoesn <60645387+BamgoeSN@users.noreply.github.com>
2024-01-27 13:54:32 +0900

    Enable undotree

--- init.lua
+++ init.lua
@@ -131,6 +131,16 @@ require('lazy').setup({
     },
   },
 
+  -- Undotree
+  {
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>ut", "<cmd>lua require('undotree').toggle()<cr>", desc = "[U]ndo[T]ree" },
+    },
+  },
+
   -- {
   --   -- Nord theme
   --   'shaunsingh/nord.nvim',
