e8fb58366a07da98b28954a19728dd5e5af53981
bamgoesn <60645387+BamgoeSN@users.noreply.github.com>
2024-04-05 13:34:45 +0900

    Add nvim colorizer

--- init.lua
+++ init.lua
@@ -805,6 +805,14 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  {
+    -- Display colors of color codes
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end
+  },
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
