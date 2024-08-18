0590b7dd040ceb87258457436885431ec7559c6e
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2024-08-18 18:33:37 +0900

    Add basic formatters to conform.nvim

--- init.lua
+++ init.lua
@@ -634,7 +634,7 @@ require('lazy').setup({
       },
     },
     opts = {
-      notify_on_error = false,
+      notify_on_error = true,
       -- format_on_save = function(bufnr)
       --   -- Disable "format_on_save lsp_fallback" for languages that don't
       --   -- have a well standardized coding style. You can add additional
@@ -647,6 +647,8 @@ require('lazy').setup({
       -- end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        python = { 'ruff' },
+        rust = { 'rustfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
