2cf0df08f2bc2da1cf4f80f6f0840cafcb9cf8a5
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2024-09-02 14:53:14 +0900

    Change inlay hint toggle to <leader>ih

--- lua/bamgoesn/plugins/lspconfig.lua
+++ lua/bamgoesn/plugins/lspconfig.lua
@@ -110,7 +110,7 @@ return {
         -- The following code creates a keymap to toggle inlay hints in your
         -- code, if the language server you are using supports them
         if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-          map('<leader>th', function()
+          map('<leader>ih', function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
           end, '[T]oggle Inlay [H]ints')
         end
